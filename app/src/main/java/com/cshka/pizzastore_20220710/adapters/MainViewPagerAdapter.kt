8f60a0c194588975dff9c0c084c658aa33696283
shchoi <gllapia@naver.com>
2022-07-16 16:02:33 +0900

    치킨가게 viewPager 추가 20220716 토

--- app/src/main/java/com/cshka/pizzastore_20220710/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/cshka/pizzastore_20220710/adapters/MainViewPagerAdapter.kt
@@ -3,18 +3,22 @@ package com.cshka.pizzastore_20220710.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.cshka.pizzastore_20220710.fragments.ChickenFragment
 import com.cshka.pizzastore_20220710.fragments.ProfileFragment
 import com.cshka.pizzastore_20220710.fragments.PizzaFragment
 
 class MainViewPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm) {
     override fun getCount(): Int {
-        return 2
+        return 3
     }
 
     override fun getItem(position: Int): Fragment {
         when(position) {
             0 -> {
                 return PizzaFragment()
+            }
+            1 -> {
+                return ChickenFragment()
             }
             else -> {
                 return ProfileFragment()
@@ -26,6 +30,9 @@ class MainViewPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm) {
         when(position) {
             0 -> {
                 return "피자 가게"
+            }
+            1 -> {
+                return "치킨 가게"
             }
             else -> {
                 return "내 정보 설정"
