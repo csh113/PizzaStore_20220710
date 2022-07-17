898d335369aee6ca5cfe0ace0ad34682c43f7c71
shchoi <gllapia@naver.com>
2022-07-17 10:14:49 +0900

    SimpleRatingBar Lib 실행 20220717 일

--- app/src/main/java/com/cshka/pizzastore_20220710/adapters/StoreListAdapter.kt
+++ app/src/main/java/com/cshka/pizzastore_20220710/adapters/StoreListAdapter.kt
@@ -12,6 +12,7 @@ import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.cshka.pizzastore_20220710.R
 import com.cshka.pizzastore_20220710.StoreData
+import com.willy.ratingbar.ScaleRatingBar
 import de.hdodenhof.circleimageview.CircleImageView
 
 class StoreListAdapter (
@@ -30,7 +31,15 @@ class StoreListAdapter (
         val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
         val scoreTxt = row.findViewById<TextView>(R.id.scoreTxt)
         val storeLogoImg = row.findViewById<ImageView>(R.id.storeLogoImg)
-        val storeRatingBar = row.findViewById<RatingBar>(R.id.storeRatingBar)
+//        val storeRatingBar = row.findViewById<RatingBar>(R.id.storeRatingBar)
+        /*에러 남
+        * java.lang.ClassCastException: com.willy.ratingbar.ScaleRatingBar cannot be cast to android.widget.RatingBar
+        * RatingBar : Linear 기준임
+        * 에러메시지 : Casting~~~ 으로 나오면 자료형이 맞지 않는 것임....
+        * */
+//        CircleImageView의 경우 ImageView를 상속받아 활용하기에 ImageView 자료형으로 Casting 가능
+//        그러나 ScaleRatingBar의 경우 상속이 BaseRatingBar라는 자체 제작 클래스이므로 RatingBar로 Casting 불가능
+        val storeRatingBar = row.findViewById<ScaleRatingBar>(R.id.storeRatingBar)
 
         nameTxt.text = mList[position].name
         scoreTxt.text = "(${mList[position].score})"
